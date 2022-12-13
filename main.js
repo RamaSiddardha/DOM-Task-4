d1be89f0183260b59bc9fd49e8b8d1cba2bdf7b4
RmaSiddardha <ramasiddardha@gmail.com>
2022-12-13 15:58:30 +0530

    Task-7

--- main.js
+++ main.js
@@ -13,10 +13,33 @@
 // console.log(list)
 // list[0].style.color='red'
 
-const items=document.querySelectorAll('li')
-items[1].style.color='Green'
-const items_2=document.querySelectorAll('li:nth-child(odd)')
-items_2.forEach((element)=>{
-    element.style.background='#a7fc08'
-})
+// const items=document.querySelectorAll('li')
+// items[1].style.color='Green'
+// const items_2=document.querySelectorAll('li:nth-child(odd)')
+// items_2.forEach((element)=>{
+//     element.style.background='#a7fc08'
+// })
 
+var newDiv=document.createElement('div')
+newDiv.className='hello'
+newDiv.id='hello1'
+newDiv.setAttribute('type','text')
+
+
+var firstText=document.createTextNode('Hello')
+console.log(firstText)
+newDiv.appendChild(firstText)
+
+
+var container=document.querySelector('header .container')
+
+
+var h1=document.querySelector('h1')
+
+container.insertBefore(newDiv,h1)
+
+var container2=document.querySelector('.container .list-group')
+
+var li1=document.querySelectorAll('.list-group-item')
+
+container2.insertBefore(newDiv,li1[0])
