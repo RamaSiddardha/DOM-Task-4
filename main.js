24098d7978dd118780beb6bf4a8cd4728168506c
RmaSiddardha <ramasiddardha@gmail.com>
2022-12-13 13:56:14 +0530

    task=6A

--- main.js
+++ main.js
@@ -1,14 +1,22 @@
-const items=document.querySelectorAll('li');
-items[2].style.backgroundColor='green'
-items[0].style.fontWeight='bold'
-items[1].style.fontWeight='bold'
-items[2].style.fontWeight='bold'
-items[3].style.fontWeight='bold'
+// const items=document.querySelectorAll('li');
+// items[2].style.backgroundColor='green'
+// items[0].style.fontWeight='bold'
+// items[1].style.fontWeight='bold'
+// items[2].style.fontWeight='bold'
+// items[3].style.fontWeight='bold'
 
-const li=document.getElementsByTagName('li')
-li[4].style.backgroundColor='gray'
-li[1].style.backgroundColor='green'
-li[2].style.display='none'
-const list=document.getElementsByClassName('tem')
-console.log(list)
-list[0].style.color='red'
+// const li=document.getElementsByTagName('li')
+// li[4].style.backgroundColor='gray'
+// li[1].style.backgroundColor='green'
+// li[2].style.display='none'
+// const list=document.getElementsByClassName('tem')
+// console.log(list)
+// list[0].style.color='red'
+
+const items=document.querySelectorAll('li')
+items[1].style.color='Green'
+const items_2=document.querySelectorAll('li:nth-child(odd)')
+items_2.forEach((element)=>{
+    element.style.background='#a7fc08'
+})
+
