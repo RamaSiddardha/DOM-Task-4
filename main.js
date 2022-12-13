408675927dd74c1d180f81a6c631a7a0b1215021
RmaSiddardha <ramasiddardha@gmail.com>
2022-12-13 13:26:58 +0530

    Task5

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ items[1].style.fontWeight='bold'
 items[2].style.fontWeight='bold'
 items[3].style.fontWeight='bold'
 
+const li=document.getElementsByTagName('li')
+li[4].style.backgroundColor='gray'
+const list=document.getElementsByClassName('tem')
+console.log(list)
+list[0].style.color='red'
